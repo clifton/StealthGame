69f7fc055b6314227378baa20a4a1866a1d61e68
Clifton King <cliftonk@gmail.com>
2019-11-10 02:18:26 -0500

    replicate isCarryingObjective

--- Source/FPSGame/Private/FPSCharacter.cpp
+++ Source/FPSGame/Private/FPSCharacter.cpp
@@ -7,6 +7,7 @@
 #include "Components/CapsuleComponent.h"
 #include "Components/PawnNoiseEmitterComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
 
 
 AFPSCharacter::AFPSCharacter()
@@ -126,4 +127,15 @@ void AFPSCharacter::MoveRight(float Value)
 		// add movement in that direction
 		AddMovementInput(GetActorRightVector(), Value);
 	}
+}
+
+void AFPSCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// gets sent to all clients
+	DOREPLIFETIME(AFPSCharacter, bIsCarryingObjective);
+
+	// only gets sent to controlling client
+	// DOREPLIFETIME_CONDITION(AFPSCharacter, bIsCarryingObjective, COND_OwnerOnly);
 }
