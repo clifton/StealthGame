8a01c7a6038dfdc279d42502960e3c3b811c91d0
Clifton King <cliftonk@gmail.com>
2019-11-10 03:08:44 -0500

    added game over / mission complete for both players

--- Source/FPSGame/Private/FPSGameState.cpp
+++ Source/FPSGame/Private/FPSGameState.cpp
@@ -1,18 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPSGameState.h"
-
-
+#include "FPSPlayerController.h"
 
 
 void AFPSGameState::MulticastOnMissionCompleted_Implementation(APawn* InstigatorPawn, bool bMissionSuccess)
 {
-	for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; It++)
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; It++)
 	{
-		APawn* Pawn = It->Get();
-		if (Pawn && Pawn->IsLocallyControlled())
+		AFPSPlayerController* PC = Cast<AFPSPlayerController>(It->Get());
+		if (PC && PC->IsLocalController())
 		{
-			Pawn->DisableInput(nullptr);
+			PC->OnMissionCompleted(InstigatorPawn, bMissionSuccess);
+			APawn* MyPawn = PC->GetPawn();
+			if (MyPawn)
+			{
+				MyPawn->DisableInput(nullptr);
+			}
 		}
 	}
 }
