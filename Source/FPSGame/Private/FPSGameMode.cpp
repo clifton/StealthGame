ae37901e3b78addce4207d3dee7b3287485d4771
Clifton King <cliftonk@gmail.com>
2019-11-10 03:24:38 -0500

    move camera for all players when game over

--- Source/FPSGame/Private/FPSGameMode.cpp
+++ Source/FPSGame/Private/FPSGameMode.cpp
@@ -33,11 +33,13 @@ void AFPSGameMode::CompleteMission(APawn* InstigatorPawn, bool bMissionSuccess)
 		{
 			AActor* NewViewTarget = ReturnedActors[0];
 
-			APlayerController* PC = Cast<APlayerController>(InstigatorPawn->GetController());
-			if (PC && NewViewTarget)
+			for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; It++)
 			{
-				// server pushes this to client automatically
-				PC->SetViewTargetWithBlend(NewViewTarget, 0.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+				APlayerController* PC = It->Get();
+				if (PC)
+				{
+					PC->SetViewTargetWithBlend(NewViewTarget, 0.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+				}
 			}
 		}
 	}
